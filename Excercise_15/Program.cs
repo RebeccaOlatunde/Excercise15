4d32115638f686560a4beb3ab64a3e5d740b912c
RebeccaOlatunde <54245689+RebeccaOlatunde@users.noreply.github.com>
2020-04-24 17:07:18 -0400

    Update Program.cs

--- Excercise_15/Program.cs
+++ Excercise_15/Program.cs
@@ -7,6 +7,39 @@ namespace Excercise_15
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            
+             {
+                string answer;
+                do
+                {
+                    bool valid;
+                    string input;
+                    int number;
+
+                    do
+                    {
+                        Console.WriteLine("Please enter your number");
+                        input = Console.ReadLine();
+
+                        valid = int.TryParse(input, out number);
+                    }
+                    while (valid == false);
+
+                    for (int i = 1; i <= number; i++)
+                    {
+
+                        Console.WriteLine(i * i * i);
+
+                    }
+
+                    Console.WriteLine("Continue ? (y / n)");
+                    answer = Console.ReadLine().ToUpper();
+
+                }
+                while (answer == "Y");
+
+                Console.WriteLine("Goodbye!");
+            }
         }
     }
 }
